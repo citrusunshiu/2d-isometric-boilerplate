2a4e7cdd20cd2a2085dc0dacbf8ac1da8384d379
Citrus Unshiu <ntorikn@gmail.com>
2020-04-14 23:34:28 -0700

    adding movement functionality, blank test methods

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Units;
+using UnityEngine;
+using YuguLibrary.Controllers;
+using YuguLibrary.Enumerations;
+using YuguLibrary.Models;
+using YuguLibrary.Utilities;
+
+public class PlayerController : MonoBehaviour
+{
+    public Player player;
+    public Camera mainCamera;
+
+    void Awake()
+    {
+        player = new Player(mainCamera);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("pc started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.ReadInputs();
+    }
+}
---
+++ Assets/Scripts/UnitDetectorController.cs
@@ -0,0 +1,39 @@
+﻿using Instances;
+using System.Collections;
+using System.Collections.Generic;
+using Units;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using YuguLibrary.Controllers;
+using YuguLibrary.Enumerations;
+using YuguLibrary.Models;
+using YuguLibrary.Utilities;
+
+public class UnitDetectorController : MonoBehaviour
+{
+    public UnitDetector unitDetector;
+
+    public Tilemap indicators;
+    public Tilemap geography;
+
+    void Awake()
+    {
+        unitDetector = new UnitDetector(geography, indicators, this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("udc started");
+        TestUnit unit = new TestUnit(1, TargetTypes.Ally);
+        unitDetector.LoadNewInstance(new DummyRoom(), new Vector3Int(11, 0, 1));
+        UtilityFunctions.GetActiveUnitDetector().SpawnOverworldObject(unit, new Vector3Int(11, 0, 1));
+        UtilityFunctions.GetActivePlayer().SetPlayerUnit(unit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
